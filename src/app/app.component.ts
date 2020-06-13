bcbcdf3376e4d9058d24be67603e9b57968d40c3
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2020-06-13 22:09:45 +0700

    update code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Task} from './app.model';
 import {compareNumbers} from '@angular/compiler-cli/src/diagnostics/typescript_version';
 
@@ -7,10 +7,11 @@ import {compareNumbers} from '@angular/compiler-cli/src/diagnostics/typescript_v
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'test3';
   sizeTask = 10;
   taskToAdd = '';
+  taskToFilter = '';
   tasks = [
     new Task(0, 'Đi ngủ', true),
     new Task(1, 'Học bài', false),
@@ -22,12 +23,18 @@ export class AppComponent {
     new Task(7, 'Thăm ngàn', true),
     new Task(8, 'Kẹp ngần', true),
     new Task(9, 'Get money', true),
-
   ];
+  tasksFilter = [];
+
+  ngOnInit(): void {
+    this.tasksFilter = [...this.tasks];
+  }
 
   addClick() {
     // window.alert(this.tasks[0].id);
     this.tasks.push(new Task(this.sizeTask++, this.taskToAdd, true));
+    this.tasksFilter.push(this.tasks[this.tasks.length - 1]);
+    this.taskToAdd = '';
   }
 
   checkClick(id) {
@@ -40,9 +47,25 @@ export class AppComponent {
         tmp === true ? item.isActive = false : item.isActive = true;
         return;
       }});
+
   }
 
   sortClick() {
+    // console.log('sort');
     this.tasks.sort((a, b) => b.name.localeCompare(a.name));
+    this.tasksFilter.sort((a, b) => b.name.localeCompare(a.name));
+  }
+
+  filter() {
+    // console.log(this.taskToFilter);
+    this.tasksFilter = this.tasks.filter(val => val.name.toLowerCase().includes(this.taskToFilter.toLowerCase()));
+    // console.log(this.tasksFilter);
+  }
+
+  clear() {
+    // xóa nội dung input
+    this.taskToFilter = '';
+    // đặt task về task gốc
+    this.tasksFilter = [...this.tasks];
   }
 }
